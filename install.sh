76458d808ac8a6669649aa2ac7eb93df775b0664
Masaya Kazama <miyan@t-p.jp>
2021-02-11 19:52:21 +0900

    add starship config

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.zprofile ~/.zprofile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.starship.toml ~/.starship.toml
 # ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 # ln -sf ~/dotfiles/.tigrc ~/.tigrc
