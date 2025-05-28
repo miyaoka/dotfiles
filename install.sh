275f32c0cf41b2a405b07e6d434180bc4e7fc8f1
Masaya Kazama <miyaokamiyao@gmail.com>
2025-05-28 23:35:16 +0000

    各種設定をサブディレクトリ化

--- install.sh
+++ install.sh
@@ -4,24 +4,24 @@ wd=$(pwd)
 # シンボリックリンクを作成
 
 ## zshenvはrootにしか置けない
-ln -sf ${wd}/.zshenv ~/.zshenv
+ln -sf ${wd}/zsh/.zshenv ~/.zshenv
 
 ## ~/.config下に配置 
 
+### zsh（zshenvでZDOTDIR設定）
+mkdir -p ~/.config/zsh
+ln -sf ${wd}/zsh/.zshrc ~/.config/zsh/.zshrc
+ln -sf ${wd}/zsh/.zsh_aliases ~/.config/zsh/.zsh_aliases
+ln -sf ${wd}/zsh/.zsh_exports ~/.config/zsh/.zsh_exports
+ln -sf ${wd}/zsh/.zsh_functions ~/.config/zsh/.zsh_functions
+
 ### git
 mkdir -p ~/.config/git
-ln -sf ${wd}/.gitconfig ~/.config/git/config
-ln -sf ${wd}/.gitignore ~/.config/git/ignore
-
-### zsh
-mkdir -p ~/.config/zsh
-ln -sf ${wd}/.zshrc ~/.config/zsh/.zshrc
-ln -sf ${wd}/.zsh_aliases ~/.config/zsh/.zsh_aliases
-ln -sf ${wd}/.zsh_exports ~/.config/zsh/.zsh_exports
-ln -sf ${wd}/.zsh_functions ~/.config/zsh/.zsh_functions
+ln -sf ${wd}/git/.gitconfig ~/.config/git/config
+ln -sf ${wd}/git/.gitignore ~/.config/git/ignore
 
 ### starship
-ln -sf ${wd}/.starship.toml ~/.config/.starship.toml
+ln -sf ${wd}/starship/.starship.toml ~/.config/.starship.toml
 
 ### mise
 mkdir -p ~/.config/mise
