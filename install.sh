e8ec1bcd8e8ad25f29a5a67b3b385618eabf47c3
Masaya Kazama <miyaokamiyao@gmail.com>
2025-05-30 06:31:16 +0000

    フォルダ内を一括リンク

--- install.sh
+++ install.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
 wd=$(pwd)
 
+# フォルダ内のファイルを指定先にリンクする関数
+link_dir() {
+  local src_dir="$1"
+  local dest_dir="$2"
+  
+  mkdir -p "$dest_dir"
+  find "${wd}/${src_dir}" -type f | while read file; do
+    # 相対パスを取得してディレクトリ構造を保持
+    rel_path=$(echo "$file" | sed "s|${wd}/${src_dir}/||")
+    dest_file="$dest_dir/$rel_path"
+    dest_subdir=$(dirname "$dest_file")
+    
+    echo "Creating link: $file -> $dest_file"
+    mkdir -p "$dest_subdir"
+    ln -sf "$file" "$dest_file"
+  done
+}
+
 # シンボリックリンクを作成
 
 ## zshenvはrootにしか置けない
@@ -12,15 +30,7 @@ ln -sf ${wd}/zsh/.zshenv ~/.zshenv
 mkdir -p ~/.config/zsh
 ln -sf ${wd}/zsh/.zshrc ~/.config/zsh/.zshrc
 
-### git
-mkdir -p ~/.config/git
-ln -sf ${wd}/git/.gitconfig ~/.config/git/config
-ln -sf ${wd}/git/.gitignore ~/.config/git/ignore
-
-### starship
-mkdir -p ~/.config/starship
-ln -sf ${wd}/starship/starship.toml ~/.config/starship/starship.toml
-
-### mise
-mkdir -p ~/.config/mise
-ln -sf ${wd}/mise/config.toml ~/.config/mise/config.toml
+### config directories
+link_dir "git" ~/.config/git
+link_dir "starship" ~/.config/starship
+link_dir "mise" ~/.config/mise
