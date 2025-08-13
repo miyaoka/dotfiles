1c9bbc69da7a786b4e987daaa48375126b83959a
Masaya Kazama <miyaokamiyao@gmail.com>
2025-08-13 15:06:44 +0900

    script: sync時のログ表示変更

--- sync.sh
+++ sync.sh
@@ -14,7 +14,9 @@ find "$src_dir" -mindepth 1 | while read item; do
     mkdir -p "$dest_item"
   elif [ -f "$item" ]; then
     # ファイルの場合はシンボリックリンクを作成
-    echo "Creating link: $item -> $dest_item"
+    # fオプションで既存ファイルが存在していても強制上書きする
+    echo "$rel_path
+  $dest_item"
     ln -sf "$item" "$dest_item"
   fi
 done
